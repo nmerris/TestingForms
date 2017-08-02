4e9b651ca645a7ebbc141e9442e49c55932b52f8
nmerris <nate.merris@gmail.com>
2017-08-02 11:50:30 -0400

    initial commit

---
+++ src/main/java/com/nmerris/testingforms/TestingformsApplication.java
@@ -0,0 +1,12 @@
+package com.nmerris.testingforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingformsApplication.class, args);
+	}
+}
